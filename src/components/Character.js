a3130e85460a21ca0bbb7f9013ccd749e18d8314
charles johnson <dagoclmt@gmail.com>
2022-04-23 13:27:55 -0700

    MVP Reached

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from "react";
+
+const Character = props => {
+    return (
+        <div className="character">
+            {props.info.name}
+
+        </div>
+    )
+}
+export default Character;
