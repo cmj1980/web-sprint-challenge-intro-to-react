49e361d5d89b22c60b43a2061a2fad0576008182
charles johnson <dagoclmt@gmail.com>
2022-04-24 12:32:03 -0700

    MVP Reached Styled Componets Strarted

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,13 @@
 // Write your Character component here
 import React from "react";
+import styledComponents from "styled-components";
 
 const Character = props => {
     return (
         <div className="character">
-            {props.info.name}
+            <h3>{props.info.name}</h3>
+            
+            
 
         </div>
     )
